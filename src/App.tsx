9115e64dd5bfb544cf31f211a81f17b4af602a32
naser karami <naser.karami76@gmail.com>
2023-02-09 15:21:01 +0330

    fix root v.1

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import DashbordAdmin from "./admin-layout/DashbordAdmin";
 import LoginAdmin from "./admin-layout/LoginAdmin";
 import Home from "./main-layout/Home";
 import LoginUser from "./main-layout/LoginUser";
+import MainLayout from "./main-layout/MainLayout";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
 
-          <Route path="/Login" element={<LoginUser />} />
-
-          <Route path="/" element={<Home />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
             
-          </Route>
+          </Route> 
+
           <Route path="/Admin-Dashbord" element={<AdminLayout />}>
             <Route path="" element={<DashbordAdmin />} />
           </Route>
